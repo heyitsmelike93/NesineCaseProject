69ac3375ef3114b0dd47d5ac7ffb32832c0aee9a
heyitsmelike93 <heyitsmelike93@gmail.com>
2026-04-27 12:33:03 +0300

    add network module

---
+++ app/src/main/java/com/example/nesinecaseproject/di/NetworkModule.kt
@@ -0,0 +1,27 @@
+package com.example.nesinecaseproject.di
+
+import com.example.nesinecaseproject.data.remote.api.PostApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://jsonplaceholder.typicode.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    fun providePostApi(retrofit: Retrofit): PostApi {
+        return retrofit.create(PostApi::class.java)
+    }
+}
