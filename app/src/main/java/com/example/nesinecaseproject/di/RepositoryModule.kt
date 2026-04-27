da9d4c8535a9a659214639eed792dc74271188b7
heyitsmelike93 <heyitsmelike93@gmail.com>
2026-04-27 13:34:33 +0300

    add repository module

---
+++ app/src/main/java/com/example/nesinecaseproject/di/RepositoryModule.kt
@@ -0,0 +1,18 @@
+package com.example.nesinecaseproject.di
+
+import com.example.nesinecaseproject.data.repository.PostRepositoryImpl
+import com.example.nesinecaseproject.domain.repository.PostRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    abstract fun bindPostRepository(
+        impl: PostRepositoryImpl
+    ): PostRepository
+}
