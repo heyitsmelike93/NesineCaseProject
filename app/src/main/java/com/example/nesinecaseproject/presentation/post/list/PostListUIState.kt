1be95c371a3bb82c64fb5ea891d3f6911b7818e1
heyitsmelike93 <heyitsmelike93@gmail.com>
2026-04-27 22:11:44 +0300

    remove unused state attr

--- app/src/main/java/com/example/nesinecaseproject/presentation/post/list/PostListUIState.kt
+++ app/src/main/java/com/example/nesinecaseproject/presentation/post/list/PostListUIState.kt
@@ -5,6 +5,5 @@ import com.example.nesinecaseproject.domain.model.Post
 data class PostListUIState(
     val isLoading: Boolean = false,
     val posts: List<Post> = emptyList(),
-    val selectedPost: Post? = null,
     val error: String? = null
 )
