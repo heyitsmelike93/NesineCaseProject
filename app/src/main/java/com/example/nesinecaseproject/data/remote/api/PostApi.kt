47c8b326af7b61d91a4ed34706bb6b0276518ab9
heyitsmelike93 <heyitsmelike93@gmail.com>
2026-04-27 16:23:09 +0300

    add post detail api

--- app/src/main/java/com/example/nesinecaseproject/data/remote/api/PostApi.kt
+++ app/src/main/java/com/example/nesinecaseproject/data/remote/api/PostApi.kt
@@ -2,10 +2,16 @@ package com.example.nesinecaseproject.data.remote.api
 
 import com.example.nesinecaseproject.data.remote.dto.PostDto
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface PostApi {
 
     @GET("posts")
     suspend fun getPosts(): List<PostDto>
+
+    @GET("posts/{id}")
+    suspend fun getPostById(
+        @Path("id") id: Int
+    ): PostDto
 
 }
